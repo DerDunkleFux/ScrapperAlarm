29ade1e9e9eb383971b6ae076ab0db0986369473
DerDunkleFux <99053098+DerDunkleFux@users.noreply.github.com>
2026-06-09 11:25:05 -0500

    Add startDate property to Alarm to pass date to frontend in case day of alarm and currentday are different

--- app/app/components/AlarmCard.vue
+++ app/app/components/AlarmCard.vue
@@ -1,33 +1,115 @@
 <template>
-    <v-container>
-        <div class="d-flex justify-start flex-wrap bg-green ">
-            <v-row>
-                <v-col cols="6" class="text-title-large pa-2">
-                    {{ name }}
-                </v-col>
-                <v-col cols="6" class="text-header-large pa-2">
-                    <div>
-                        Some other options here
+    <!-- <v-container> -->
+    <!-- Outer caller has to have v-container -->
+    <!-- <div class="h-50 w-50 rounded-lg inset-shadow-sm/50 inset-shadow-white">
+        test
+     </div> -->
+<!-- inset-shadow-sm/50 inset-shadow-sm/50 inset-shadow-white -->
+    <div class="rounded-lg ">
+ <!-- :hover="isHover"  -->
+        <v-card :variant="cardVariant" class="rounded-lg">
+            <v-card-item class="bg-slate-800" prepend-icon="mdi-clock" :title="name">
+                <template v-slot:append>
+                    <div class="text-body-small px-2">
+                        {{ startDate?.getDate() }}/{{ startDate?.getMonth() }}/{{ startDate?.getFullYear() }}
                     </div>
-                    <div>
-                        Some Options here
-                    </div>
-                </v-col>
-                <v-col class="text-title-large pa-2">
-                    {{ start }}
-                </v-col>
-                <v-col class="text-title-large pa-2">
-                    {{ end }}
-                </v-col>
-            </v-row>
-        </div>
-    </v-container>
+                </template>
+            </v-card-item>
+            <!-- <v-card-title class="px-4 bg-slate-800 ">
+
+            <div class="text-title-large">
+                {{ name }}
+            </div>
+        </v-card-title> -->
+            <div class="bg-sky-900 px-4">
+
+                <v-card-text class="pa-2 ">
+
+                    <v-row class="text-nowrap">
+                        <v-col cols="12" sm="6" class="text-title-large text-center">
+                            <div>
+                                {{ start }}
+                            </div>
+                            <div class="text-title-small text-center">Start</div>
+                        </v-col>
+                        <v-col cols="12" sm="6" class="text-title-large text-center ">
+                            <div>
+                                {{ end }}
+                            </div>
+                            <div class="text-title-small text-center">End</div>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-card-actions class="text-header-large d-flex justify-center">
+                    <v-btn class="bg-error">Delete</v-btn>
+                    <v-btn v-tooltip="activeTooltipProps" v-if="isActive" @click="toggleActive"
+                        class="bg-success">Active</v-btn>
+                    <v-btn v-tooltip="inactiveTooltipProps" v-else @click="toggleActive"
+                        class="bg-warning">Inactive</v-btn>
+                </v-card-actions>
+            </div>
+        </v-card>
+    </div>
+    <!-- </v-container> -->
 </template>
 
 <script setup lang="ts">
+import { useVariant, type Variant } from 'vuetify/lib/composables/variant.mjs'
 const props = defineProps({
     name: { type: String, required: true },
     start: { type: String, required: true },
     end: { type: String, required: true },
+    startDate: Date
 })
+
+const tooltipProps = {
+    openDelay: 400,
+}
+
+const inactiveTooltipProps = {
+    ...tooltipProps,
+    text: "Activate"
+}
+const activeTooltipProps = {
+    ...tooltipProps,
+    text: "Deactivate"
+}
+const cardVariant: Ref<Variant> = ref("text") //if inactive change to plain
+const isActive: Ref<boolean> = ref(true)
+const isHover: Ref<boolean> = ref(true)
+
+function toggleActive() {
+    console.log("Toggling active")
+    if (cardVariant.value == "text") {
+        cardVariant.value = "plain"
+    } else {
+        cardVariant.value = "text"
+    }
+    isActive.value = !isActive.value
+    isHover.value = !isHover.value
+}
+//  Alarm sounding logic
+const currDate = new Date()
+const { hours: startHours, minutes: startMinutes } = formatTime(props.start)
+const { hours: endHours, minutes: endMinutes } = formatTime(props.end)
+const alarmStart: Date = new Date()
+// TODO get date from each alarm from backend for day, month in case different than today
+// Currently only check if alarm hours & minutes is before currentDate set for next day, else set for same day
+const testDate = props.startDate
+console.log("testDate in alarmCard: ", testDate)
+function formatTime(s: string): { hours: Number, minutes: Number } {
+    let hours = 0
+    let minutes = 0
+    if (s.toLowerCase().includes("am")) {
+        hours = Number(s.replace(" ", "").replace(/am/i, "").replace(/:(\d+)/, ""))
+        minutes = Number(s.replace(" ", "").replace(/am/i, "").replace(/(\d+):/, ""))
+    }
+    else {
+        hours = Number(s.replace(" ", "").replace(/pm/i, "").replace(/:(\d+)/, ""))
+        minutes = Number(s.replace(" ", "").replace(/pm/i, "").replace(/(\d+):/, ""))
+    }
+    console.log("Got hours: ", hours)
+    console.log("Got minutes: ", minutes)
+    return { hours, minutes }
+}
 </script>
--- app/app/pages/alarm.vue
+++ app/app/pages/alarm.vue
@@ -21,9 +21,10 @@
         <!-- All Alarms -->
 
 
-        <v-row justify="space-between" class="bg-orange">
-            <v-col cols="12" md="6" v-for="alarm in alarms" class="bg-black">
-                <alarm-card :name="alarm.name" :end="alarm.end || ''" :start="alarm.start || ''"></alarm-card>
+        <v-row justify="space-evenly" >
+            
+            <v-col cols="6" md="3" v-for="alarm in alarms" >
+                <alarm-card :name="alarm.name" :end="alarm.end || ''" :start="alarm.start || ''" :start-date="currDate"></alarm-card>
                 <!-- </div> -->
             </v-col>
         </v-row>
@@ -39,14 +40,15 @@ import AlarmCard from '~/components/AlarmCard.vue';
 
 const userName = ref("Manuel L Jackson")
 const route = useRoute()
-
+const currDate = new Date()
+console.log("CurrDate in Alarm: ", currDate)
 const alarms: Alarm[] = [
-    { name: "Alarm 1", start: "8:00 AM", end: "9:00 AM" },
-    { name: "Alarm 2", start: "10:00 AM", end: "11:00 AM" },
-    { name: "Alarm 3", start: "12:00 PM", end: "01:00 PM" },
-    { name: "Alarm 3", start: "12:00 PM", end: "01:00 PM" },
-    { name: "Alarm 4", start: "02:00 PM", end: "03:00 PM" },
-    { name: "Alarm 4", start: "02:00 PM", end: "03:00 PM" },
+    { name: "Alarm 1", start: "8:00 AM", end: "9:00 AM"  , startDate: currDate },
+    { name: "Alarm 2", start: "10:15 AM", end: "11:30 AM", startDate: currDate },
+    { name: "Alarm 3", start: "12:00 PM", end: "01:00 PM", startDate: currDate },
+    { name: "Alarm 4", start: "12:00 PM", end: "01:00 PM", startDate: currDate },
+    { name: "Alarm 5", start: "02:00 PM", end: "03:00 PM", startDate: currDate },
+    { name: "Alarm 6", start: "02:00 PM", end: "03:00 PM", startDate: currDate },
 ]
 
 console.log("got route: ", route)
